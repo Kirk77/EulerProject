b5a849375e7f359d0e1ccea551e96a44da2498b5
Kirk77 <kirk77@gmail.com>
2016-12-06 19:50:47 +0900

    62

--- kr/kirk/euler/p000/Problem062.java
+++ kr/kirk/euler/p000/Problem062.java
@@ -1,5 +1,8 @@
 package kr.kirk.euler.p000;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 
 The cube, 41063625 (3453), can be permuted to produce two other cubes: 56623104 (3843) and 66430125 (4053).
@@ -17,12 +20,66 @@ public class Problem062 {
 
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
-		System.out.println(solution(3));
-//		System.out.println(solution(5));
+//		System.out.println(solution(3));
+		System.out.println(solution(5));
 		System.out.println("실행시간 : " + (System.currentTimeMillis() - startTime) + "ms");
 	}
 
 	private static long solution(int x) {
-		return -1;
+		List<Long> result = new ArrayList<Long>();
+		for (long i=345;;i++){
+			result.clear();
+			result.add(i);
+			findCube(result,x);
+			if (result.size()==x) {
+				print(result);
+				return i*i*i;
+			}
+		}
+	}
+
+	private static boolean findCube(List<Long> result, int x) {
+		if ( result.size() == x) return true;
+		long i = result.get(result.size()-1);
+		for ( long k = i+1;;k++) {
+
+			long kkk = k*k*k;
+			long iii = i*i*i;
+			int lk = String.valueOf(kkk).length();
+			int li = String.valueOf(iii).length();
+			if ( lk > li) return false;
+			
+			if ( checkNumbers(iii, kkk) && !result.contains(k)) {
+				result.add(k);
+				if ( findCube(result, x) == false){
+					result.remove(result.size()-1);
+				}
+			}
+		}
+	}
+
+	private static void print(List<Long> result) {
+		for ( long t : result) System.out.println(t + "  " + t*t*t);
+		System.out.println();
+	}
+
+	private static boolean checkNumbers(long c, long cc) {
+		int[] p1 = countPosition(c);
+		int[] p2 = countPosition(cc);
+		
+		for ( int i=0; i<10; i++) if ( p1[i] != p2[i]) return false;
+		return true;
+	}
+
+	private static int[] countPosition(long c) {
+		int[] p = new int[10];
+		long d = c;
+		int r = 0;
+		while(d!=0) {
+			r = (int) (d%10);
+			d /=10;
+			p[r]++;
+		}
+		return p;
 	}
 }
---
+++ kr/kirk/euler/p000/Problem063.java
@@ -0,0 +1,22 @@
+package kr.kirk.euler.p000;
+
+/*
+The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+How many n-digit positive integers exist which are also an nth power?
+
+
+다섯 자리 숫자인 16807은 75으로 5제곱수입니다. 또, 아홉 자리 숫자인 134217728은 89으로 9제곱수입니다.
+n자리 숫자이면서 n제곱수도 되는 양의 정수는 모두 몇 개나 있습니까?
+ */
+public class Problem063 {
+
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		System.out.println(solution());
+		System.out.println("실행시간 : " + (System.currentTimeMillis() - startTime) + "ms");
+	}
+
+	private static long solution() {
+		return -1;
+	}
+}
